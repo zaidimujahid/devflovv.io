29539ccfbb9c542ea1d7ccd12d82836b5b0f8602
mujahid <zaidimujahid60@gmail.com>
2022-04-14 10:06:46 -0800

    1st time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devflovv';
+  myimage:string = "/src/image/Final logo.jpg";
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { ServiceComponent } from './service/service.component';
+import { WorkComponent } from './work/work.component';
+import { CareerComponent } from './career/career.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    ServiceComponent,
+    WorkComponent,
+    CareerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
