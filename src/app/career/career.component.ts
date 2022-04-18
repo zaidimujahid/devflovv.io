6bbaa255fbfcfcb841f389c3733d09b6d8992b50
mujahid <zaidimujahid60@gmail.com>
2022-04-18 02:06:38 -0800

    day-3

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -3,38 +3,62 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
-  styleUrls: ['./career.component.css']
+  styleUrls: ['./career.component.css'],
 })
 export class CareerComponent implements OnInit {
-  isUxui = false;
-  toggleUxui(){
-    this.isUxui = !this.isUxui;
-  }
-
+  isUxui = true;
   isTesting = false;
-  toggleTesting(){
-    this.isTesting = !this.isTesting;
-  }
-
   isChain = false;
-  toggleChain(){
-    this.isChain = !this.isChain;
-  }
-  isMobile = false;
-  toggleMobile(){
-    this.isMobile = !this.isMobile;
-  }
   isWeb = false;
-  toggleWeb(){
-    this.isWeb = !this.isWeb;
-  }
-  isLot = false;
-  toggleLot(){
-    this.isLot = !this.isLot;
-  }
-  constructor() { }
+  isMobile = false;
+  isIot = false;
 
-  ngOnInit(): void {
+  toggleCard(selection: string) {
+    if (selection === 'UIUX') {
+      this.isUxui = true;
+      (this.isTesting = false),
+        (this.isChain = false),
+        (this.isWeb = false),
+        (this.isMobile = false),
+        (this.isIot = false);
+    } else if (selection === 'testing') {
+      this.isTesting = true;
+      (this.isUxui = false),
+        (this.isChain = false),
+        (this.isWeb = false),
+        (this.isMobile = false),
+        (this.isIot = false);
+    } else if (selection === 'blockchain') {
+      this.isChain = true;
+      this.isUxui = false;
+      (this.isTesting = false),
+        (this.isWeb = false),
+        (this.isMobile = false),
+        (this.isIot = false);
+    } else if (selection === 'mobile') {
+      this.isMobile = true;
+      (this.isUxui = false),
+        (this.isTesting = false),
+        (this.isWeb = false),
+        (this.isChain = false),
+        (this.isIot = false);
+    } else if (selection === 'web') {
+      this.isWeb = true;
+      (this.isUxui = false),
+        (this.isTesting = false),
+        (this.isMobile = false),
+        (this.isChain = false),
+        (this.isIot = false);
+    } else if (selection === 'IoT') {
+      this.isIot = true;
+      (this.isUxui = false),
+        (this.isTesting = false),
+        (this.isMobile = false),
+        (this.isChain = false),
+        (this.isWeb = false);
+    }
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
